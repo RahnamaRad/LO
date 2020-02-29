d13d3abff8df2f0bab6c163d440fe6e2f9a0d65b
RahnamaRad <43912226+RahnamaRad@users.noreply.github.com>
2020-02-29 11:26:54 -0500

    Add files via upload

---
+++ Fig1_ALOvsKfold.R
@@ -0,0 +1,106 @@
+rm(list = ls())    #delete objects
+cat("\014")
+library(class)
+library(ggplot2)
+library(dplyr)
+library(glmnet)
+set.seed(0)
+
+# generate data
+p              =     2000
+n              =     500
+k              =     100
+beta.star      =     rep(0, p)
+beta.star[1:k] =     sqrt(5*10/9)/sqrt(k)#(2*rbinom(k,1,0.5)-1) 
+o              =     sqrt(2)#0.7 * sqrt(k/n)
+MCMCsamples    =     100
+m              =     20
+aa             =     0.5
+
+
+for (i in 1:MCMCsamples){
+  X              =     matrix(rnorm(n*p, mean = 0, sd = 1), ncol = p, nrow = n)
+  e              =     rnorm(n, mean = 0, sd = o)
+  y              =     X %*% beta.star + e
+  if (i==1){
+    cv.lasso.3f    =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, dfmax = floor(0.99 * n), nfolds = 3)
+    cv.lasso.5f    =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, dfmax = floor(0.99 * n), nfolds = 5)
+    cv.lasso.10f   =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, dfmax = floor(0.99 * n), nfolds = 7)
+    cv.lasso.lo    =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, dfmax = floor(0.99 * n), nfolds = n)
+    lasso.fit      =     glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, dfmax = floor(0.99 * n))
+    lambda.min     =     max(min(lasso.fit$lambda), min(cv.lasso.3f$lambda), min(cv.lasso.5f$lambda), min(cv.lasso.10f$lambda), min(cv.lasso.lo$lambda))
+    lambda.max     =     min(max(lasso.fit$lambda), max(cv.lasso.3f$lambda), max(cv.lasso.5f$lambda), max(cv.lasso.10f$lambda), max(cv.lasso.lo$lambda))
+    lambda         =     rev(exp(seq(log(lambda.min), log(lambda.max), length.out = m)))
+    cv.3f          =     matrix(rep(0, m*MCMCsamples), nrow = m, ncol = MCMCsamples)
+    cv.5f          =     matrix(rep(0, m*MCMCsamples), nrow = m, ncol = MCMCsamples)
+    cv.10f         =     matrix(rep(0, m*MCMCsamples), nrow = m, ncol = MCMCsamples)
+    cv.lo          =     matrix(rep(0, m*MCMCsamples), nrow = m, ncol = MCMCsamples)
+    extraErr       =     matrix(rep(0, m*MCMCsamples), nrow = m, ncol = MCMCsamples)
+  }
+  cv.lasso.3f     =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, lambda = lambda, nfolds = 3)
+  cv.lasso.5f     =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, lambda = lambda, nfolds = 5)
+  cv.lasso.10f    =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, lambda = lambda, nfolds = 7)
+  cv.lasso.lo     =     cv.glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, lambda = lambda, nfolds = n)
+  lasso.fit       =     glmnet(X, y, alpha = aa,  intercept = FALSE, standardize = FALSE, lambda = lambda)
+  
+  # note that the cv.glmnet doesn't necessarily calculate CV over the values of lambdas given to the function.
+  # to test this claim compare the set lambdas feeded to the cv.glmnet function and the lambdas of the object provided by cv.glmnet.
+  # this creates a problem because in each iteration we have the CV evaluate over a slightly different set of lambdas
+  # in order to fix this problem we use a very flexible smoothing spline to evaluate the CV over a fixed set of lambdas
+  # note that since the df=number of lambdas -1, the amount of smoothing is minimal and won't change our results in a meaningful manner
+  cv.3f.sspline   =     smooth.spline(cv.lasso.3f$lambda, cv.lasso.3f$cvm, df = length(cv.lasso.3f$lambda)-1) 
+  cv.3f.i         =     predict(cv.3f.sspline, lambda)
+  cv.3f[ ,i]      =     cv.3f.i$y
+    
+  cv.5f.sspline   =     smooth.spline(cv.lasso.5f$lambda, cv.lasso.5f$cvm, df = length(cv.lasso.5f$lambda)-1) 
+  cv.5f.i         =     predict(cv.5f.sspline, lambda)
+  cv.5f[ ,i]      =     cv.5f.i$y
+  
+  cv.10f.sspline  =     smooth.spline(cv.lasso.10f$lambda, cv.lasso.10f$cvm, df = length(cv.lasso.10f$lambda)-1) 
+  cv.10f.i        =     predict(cv.10f.sspline, lambda)
+  cv.10f[ ,i]     =     cv.10f.i$y
+  
+  cv.lo.sspline   =     smooth.spline(cv.lasso.lo$lambda, cv.lasso.lo$cvm, df = length(cv.lasso.lo$lambda)-1)
+  cv.lo.i         =     predict(cv.lo.sspline, lambda)
+  cv.lo[ ,i]      =     cv.lo.i$y
+  
+  extraErr[ ,i]   =     colSums((lasso.fit$beta - beta.star)^2) + o^2
+  print(i)
+}
+
+cv.3f.mean     =     rowMeans(cv.3f)
+cv.3f.se       =     sqrt(apply(cv.3f, 1, var))/sqrt(MCMCsamples)
+cv.5f.mean     =     rowMeans(cv.5f)
+cv.5f.se       =     sqrt(apply(cv.5f, 1, var))/sqrt(MCMCsamples)
+cv.10f.mean    =     rowMeans(cv.10f)
+cv.10f.se      =     sqrt(apply(cv.10f, 1, var))/sqrt(MCMCsamples)
+cv.lo.mean     =     rowMeans(cv.lo)
+cv.lo.se       =     sqrt(apply(cv.lo, 1, var))/sqrt(MCMCsamples)
+extraErr.mean  =     rowMeans(extraErr)
+extraErr.se    =     sqrt(apply(extraErr, 1, var))/sqrt(MCMCsamples)
+
+error          =     data.frame(c( rep("5 fold CV", m), rep("LO", m),   rep("extraErr", m) ), n*c(lambda, lambda, lambda) ,c(cv.5f.mean, cv.lo.mean, extraErr.mean),c(cv.5f.se, cv.lo.se, extraErr.se)*sqrt(MCMCsamples))
+colnames(error)=     c("method", "lambda", "err", "sd")
+error.plot     =     ggplot(error, aes(x=lambda, y = err, color=method)) +   geom_line(size=0.5) + scale_x_log10() + theme(legend.text = element_text(colour="black", size=10, face="bold")) + geom_pointrange(aes(ymin=err-sd, ymax=err+sd),  size=0.4,  shape=20) +  theme(legend.title=element_blank())
+
+
+eror           =     data.frame(c( rep("3 fold CV", m), rep("5 fold CV", m), rep("7 fold CV", m), rep("LO", m),   rep("outErr", m) ), n*c(lambda, lambda, lambda, lambda, lambda) ,c(cv.3f.mean, cv.5f.mean, cv.10f.mean, cv.lo.mean, extraErr.mean),c(cv.3f.se, cv.5f.se, cv.10f.se, cv.lo.se, extraErr.se))
+colnames(eror) =     c("method", "lambda", "err", "se")
+#eror           =     read.table("eror.txt")
+
+eror.plot      =     ggplot(eror, aes(x=lambda, y = err, color=method)) +   geom_line(size=0.5) 
+#eror.plot      =     eror.plot  + scale_x_log10(breaks = c(seq(10,300,30)))   
+eror.plot      =     eror.plot  + theme(legend.text = element_text(colour="black", size=10, face="bold", family = "Arial")) 
+eror.plot      =     eror.plot  + geom_pointrange(aes(ymin=err-se, ymax=err+se),  size=0.4,  shape=15)
+eror.plot      =     eror.plot  + theme(legend.title=element_blank()) 
+eror.plot      =     eror.plot  + scale_color_discrete(breaks=c("3 fold CV","5 fold CV","7 fold CV","LO","outErr"))
+eror.plot      =     eror.plot  + theme(axis.title.x = element_text(size=18)) 
+eror.plot      =     eror.plot  + theme(axis.title.y = element_text(size=14, face="bold", family = "Arial")) 
+eror.plot      =     eror.plot  + xlab( expression(paste( lambda))) + ylab("error")
+eror.plot      =     eror.plot  + ggtitle("Out-of-sample error \n versus \n k-fold cross validation")
+eror.plot      =     eror.plot  + theme(plot.title = element_text(hjust = 0.5, vjust = -10,size=10, face="bold",  family = "Arial"))
+eror.plot      =     eror.plot  + theme(legend.position = c(1,0), legend.justification = c(1,-0.5))
+eror.plot
+
+
+write.table(eror, "eror.txt", sep="\t")
---
+++ Table2_RidgeLogistic.R
@@ -0,0 +1,81 @@
+rm(list = ls())    #delete objects
+cat("\014")
+library(class)
+library(ggplot2)
+library(gridExtra)
+library(dplyr)
+library(glmnet)
+library(alocv)
+library(rmutil)
+library(tictoc)
+library(scales)
+set.seed(1)
+# generate data
+p_               =     seq(100, 1100, 200)
+delta            =     1
+alpha_elnet      =     0
+MCMCsamples      =     100
+lo               =     matrix(rep(0, MCMCsamples* length(p_))  ,MCMCsamples, length(p_))
+outErr           =     matrix(rep(0, MCMCsamples* length(p_))  ,MCMCsamples, length(p_))
+MSE              =     rep(0,length(p_))
+bias             =     rep(0,length(p_))
+
+
+a                =   0
+R2               =   0
+
+for (i in 1:length(p_)){
+  p              =     p_[i] 
+  n              =     p * delta
+  for (s in 1:MCMCsamples){
+    beta.star      =     rep(1, p)
+    X              =     matrix(rnorm( n*p, mean = 0, sd = 1/sqrt(n) ), nrow = n, ncol = p)
+    py             =     exp(X %*% beta.star) / (1 + exp(X %*% beta.star))
+    y              =     rep(0, n)
+    for (ss in 1:n){
+      y[ss]=rbinom(1,1,py[ss])
+    }
+    lambdaS         =     c(0.01,0.1)/n
+    lo.glm          =     cv.glmnet(X, y, family = "binomial", alpha = alpha_elnet,  intercept = FALSE, standardize = FALSE, lambda = lambdaS, nfolds = n, type.measure = "deviance")
+    fit             =     glmnet(X, y, family = "binomial", alpha = alpha_elnet,  intercept = FALSE, standardize = FALSE, lambda = lo.glm$lambda[1])
+    lo[s, i]        =     lo.glm$cvm[1]/2
+    
+    dz              =      0.005
+    oz              =      sqrt(sum(beta.star^2)/n) 
+    z               =      seq(-5*oz, 5*oz, dz)
+    Dz              =      exp(-z^2/(2*oz^2))/sqrt(2*pi*oz^2) * dz
+    outErr[s, i]    =      - as.numeric(t(fit$beta) %*% beta.star) / sum(beta.star^2) * sum(z * exp(z)/(1 + exp(z)) *Dz) 
+    
+    oz              =      sqrt(sum(fit$beta^2)/n) 
+    z               =      seq(-5*oz, 5*oz, dz)
+    Dz              =      exp(-z^2/(2*oz^2))/sqrt(2*pi*oz^2) * dz
+    outErr[s, i]    =      outErr[s, i]    + sum(log(1+ exp(z)) * Dz)
+    
+    # for debugging 
+    if (FALSE) {
+      n.test         =     10000*n
+      X.test         =     matrix(rnorm( n.test*p, mean = 0, sd = 1/sqrt(n) ), nrow = n.test, ncol = p)
+      Ey.Xtest       =     exp(X.test %*% beta.star) / (1 + exp(X.test %*% beta.star))
+      bb             =     as.numeric(t(fit$beta) %*% beta.star) / sum(beta.star^2) * beta.star
+      outErr_        =     mean( -(X.test %*% fit$beta) * Ey.Xtest + log(1 + exp(X.test %*% fit$beta) ))
+      outErrr_       =     mean( -(X.test %*% bb) * Ey.Xtest + log(1 + exp(X.test %*% fit$beta) )  ) 
+      cat(sprintf("p= %1.f| s=%1.f| lo=%.5f| outErr=%.5f | outErr_sample=%.5f |outErr_sample_=%.5f | MSE =%.6f | scaling = %.2f | R2 = %.2f| outErr-lo = %.4f   \n", p, s, lo[s, i], outErr[s, i], outErr_, outErrr_, MSE[i], a, R2,bias[i] ) )
+      
+    }
+    bias[i]        =     mean(-lo[1:s,i] + outErr[1:s, i]) 
+    MSE[i]         =     mean((lo[1:s,i] - outErr[1:s, i])^2) 
+    if (i>1){
+      ls.fit         =     lm(log(MSE[1:i])~log(p_[1:i]))
+      a              =     ls.fit$coefficients[2] # should be close to -1 to get us 1/n scaling
+      R2             =     summary(ls.fit)$r.squared
+    }
+    
+    cat(sprintf("p= %4.f| s=%3.f| lo=%.5f| outErr=%.5f | MSE =%.6f | scaling = %.2f | R2 = %.2f| lo-outErr = %.4f   \n", p, s, lo[s, i], outErr[s, i], MSE[i], a, R2,bias[i] ) )
+  }
+}
+
+MSE.SE   =    apply((lo - outErr)^2, 2, sd)/sqrt(MCMCsamples)
+apply((lo - outErr)^2, 2, mean)
+
+     
+
